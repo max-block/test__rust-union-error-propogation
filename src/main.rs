c8a53dd5e9f805d32a10eb4fc891142f98d2ef97
Max Block <maxblock@pm.me>
2020-09-25 13:52:06 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::{fs, io::Error};
+use std::{fs, io::Error as IOError};
 
 #[derive(Debug)]
 enum AppError {
     EmptyFile,
-    IOError(Error),
+    IOError(IOError),
+}
+
+impl From<IOError> for AppError {
+    fn from(e: IOError) -> Self {
+        AppError::IOError(e)
+    }
 }
 
 fn analyze_file(path: String) -> Result<usize, AppError> {
-    let res = fs::read_to_string(path).map_err(|e| AppError::IOError(e))?;
+    let res = fs::read_to_string(path)?;
     if res.len() == 0 {
         Err(AppError::EmptyFile)
     } else {
